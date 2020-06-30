0f802f02c99571fc7c3e17feacdcc5ecbc34bf40
maymoonah-sulaiman <59207799+maymoonah-sulaiman@users.noreply.github.com>
2020-06-30 21:05:48 +0300

    database models and setup for Heroku
    in this version we are setting up the database models which we have three of them:
    Shipment
    Item
    and Shipment_items

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from models import setup_db
+from flask_cors import CORS
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route('/')
+    def get_greeting():
+        greeting = "Hello first app good"
+        return greeting
+
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    
